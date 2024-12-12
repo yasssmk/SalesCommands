99ee4a276b1f73d34df55d1422137a09e5e5b7e2
yasssmk <ysamkawi@gmail.com>
2024-12-12 16:22:34 +0100

    Change account table name

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -15,5 +15,8 @@ class Account(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = 'accounts'
+
     def __str__(self):
         return self.name   
