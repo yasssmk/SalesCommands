3497f9835398e980261429424801d2db005a394d
yasssmk <ysamkawi@gmail.com>
2024-12-16 11:16:17 +0100

    Add CRM folder in NavBar

--- frontend/src/menu-items/index.jsx
+++ frontend/src/menu-items/index.jsx
@@ -6,11 +6,12 @@ import chartsMap from './charts-map';
 import samplePage from './sample-page';
 import other from './other';
 import pages from './pages';
+import CRM from './crm';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [widget, applications, formsTables, chartsMap, samplePage, pages, other]
+  items: [CRM, widget, applications, formsTables, chartsMap, samplePage, pages, other]
 };
 
 export default menuItems;
