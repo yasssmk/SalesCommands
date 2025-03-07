a424e84941d55668e3e864b3c5d9301b85d9b239
yasssmk <ysamkawi@gmail.com>
2025-03-07 10:39:42 +0100

    migrations

--- backend/apps/accounts_app/accounts/views.py
+++ backend/apps/accounts_app/accounts/views.py
@@ -14,7 +14,7 @@ from .serializers import AccountSerializer
 from django.utils.translation import gettext_lazy as _
 from core.error_messages import CoreErrorMessages, AccountErrorMessages
 from datetime import datetime
-from ...core_apps.views import SignalAwareViewMixin
+from apps.core_apps.views import SignalAwareViewMixin
 
 class AccountAPIView(BaseAPIView, SignalAwareViewMixin):
     """
--- backend/apps/core_apps/views/__init__.py
+++ backend/apps/core_apps/views/__init__.py
@@ -1,4 +1,4 @@
-from .sinal_view_mixinx import SignalAwareViewMixin
+from .signal_view_mixin import SignalAwareViewMixin
 from .standar_dpt_view import StandardDepartmentChoicesView
 
 __all__=['SignalAwareViewMixin', 'StandardDepartmentChoicesView']
